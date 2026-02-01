e2640f0e40357eadf1be3059b02cd4eb7cfe0a86
Nikolai Aristov <kodistudios@mgmail.com>
2026-01-31 17:53:04 -0800

    Update production URL in Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://yoururl.com',
+  url: 'https://nikolaiaristov.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
