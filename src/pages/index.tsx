3ecb7fe74505e05552135052f91e162883feb60a
Nikolai Aristov <kodistudios@mgmail.com>
2026-02-09 22:15:00 -0800

    Refactor code for consistency and readability across multiple files, including updates to formatting, syntax, and configuration settings.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,28 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <img src="https://1drv.ms/i/c/fa8a0c5ee30de8e9/IQQlU_C2l1eXQq9Wk4MnPvryAfOMf2Sc1cKL-B6SLOEs0j0?height=1024" width="auto" height="1024" />
+          <img
+            src="https://1drv.ms/i/c/fa8a0c5ee30de8e9/IQQlU_C2l1eXQq9Wk4MnPvryAfOMf2Sc1cKL-B6SLOEs0j0?height=1024"
+            width="auto"
+            height="1024"
+          />
         </div>
       </div>
     </header>
@@ -26,11 +30,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
